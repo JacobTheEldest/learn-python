834ebbea2a16c4e33325465e3fc63736fd8d16ee
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-22 18:49:12 -0400

    fixed a find&replace mishap

--- area.py
+++ area.py
@@ -15,36 +15,36 @@ def triangle(a, b):
 def circle(a):
     return math.pi*a*a
         
-prfloat("\nWelcome to area.py!")
+print("\nWelcome to area.py!")
     
 while repeat:
-    prfloat("\nWhat shape do you want to compute the area of?")
-    prfloat("Square")
-    prfloat("Rectangle")
-    prfloat("Triangle")
-    prfloat("Circle")
-    prfloat("Type anything else to quit")
+    print("\nWhat shape do you want to compute the area of?")
+    print("Square")
+    print("Rectangle")
+    print("Triangle")
+    print("Circle")
+    print("Type anything else to quit")
     choice = input("> ")
-    prfloat("")
+    print("")
 
     if choice == "Square" or choice == "square":
         side1 = float(input('Width: '))
-        prfloat("The area of your square is:", square(side1))
+        print("The area of your square is:", square(side1))
         
     elif choice == "Rectangle" or choice == "rectangle":
         side1 = float(input('Length: '))
         side2 = float(input('Width: '))
-        prfloat("The area of your rectangle is:", rectangle(side1, side2))
+        print("The area of your rectangle is:", rectangle(side1, side2))
         
     elif choice == "Triangle" or choice == "triangle":
         height = float(input('Height: '))
         base = float(input('Base: '))
-        prfloat("The area of your triangle is:", triangle(base, height))
+        print("The area of your triangle is:", triangle(base, height))
         
     elif choice == "Circle" or choice == "circle":
         radius = float(input('Radius: '))
-        prfloat("The area of your circle is:", circle(radius))
+        print("The area of your circle is:", circle(radius))
         
     else:
         repeat = False
-        prfloat('\nGoodbye')
+        print('\nGoodbye')
